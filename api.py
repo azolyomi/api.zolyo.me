40916570b4bb8d30bc2136f541e0c24159fb0e54
azolyomi <andras.zolyomi@gmail.com>
2022-01-17 04:37:44 +0000

    refactor api.py

--- api.py
+++ api.py
@@ -2,15 +2,19 @@ from flask import Flask, jsonify, send_from_directory
 
 app = Flask(__name__)
 
-
 @app.route('/favicon.ico')
 def favicon():
     return (send_from_directory("./assets", 'favicon.ico'))
+
 @app.route('/')
 def homepage():
     return '<h3>You\'re not supposed to be here...</h3>'
+
 @app.route('/test', methods=["GET"])
 def return_json():
     return jsonify({'test': True})
+
+
+
 if __name__ == "__main__":
     app.run()
